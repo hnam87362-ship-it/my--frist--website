bea8415d8e9eadd3d4b23d1d78282d0ec8f99963
hnam87362-ship-it <hnam87362@gmail.com>
2026-08-24 09:59:10 +0300

    Add files via upload

---
+++ project5.py
@@ -0,0 +1,10 @@
+text=input("enter your name")
+reversed_text =""
+for char in text:
+ 
+   reversed_text=char +reversed_text
+   print(text)
+   print(reversed_text )
+
+text="malak"
+print(text[start:end: step])

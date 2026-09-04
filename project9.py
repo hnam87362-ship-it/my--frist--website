e1514c49427fd132388a30973a59aeb7c73e375d
hnam87362-ship-it <hnam87362@gmail.com>
2026-09-04 11:51:24 +0300

    Add files via upload

---
+++ project9.py
@@ -0,0 +1,13 @@
+number =int(input("enter a number: "))
+if number >0:
+    print(number,"is not a prime number")
+else:
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(number, "is a prime number")
+    else:
+        print(number, "is not a prime number")
